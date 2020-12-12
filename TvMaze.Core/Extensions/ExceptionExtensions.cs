b08c47793d7527a062eb2d8dff97c4d60e4ec2b3
Nestor Orest Plysyuk <dayonel@hotmail.es>
2020-12-12 23:11:47 +0100

    Added project configuration and layers (#1)

---
+++ TvMaze.Core/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TvMaze.Core.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static string Message(this Exception ex)
+        {
+            return $"{ex.Message}{GetSeparator()}{ex.InnerException}{GetSeparator()}{ex.StackTrace}";
+        }
+
+        private static string GetSeparator() => $"{Environment.NewLine}========================================{Environment.NewLine}";
+    }
+}
